54868a266db864cbe2d4ae130e5f702887706f8f
Arnab Kr Ghosh <arnab.kr.ghosh@ericsson.com>
2017-09-14 01:45:31 +0530

    Status table insert script and Get Status by Entity API commited

---
+++ sf-server/src/main/java/com/salesforce/rest/StatusResource.java
@@ -0,0 +1,55 @@
+package com.salesforce.rest;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.salesforce.model.Message;
+import com.salesforce.model.Status;
+import com.salesforce.privileges.Privilege;
+import com.salesforce.repository.StatusRepository;
+import com.salesforce.security.Secured;
+
+/**
+ * @author Arnab Kr Ghosh
+ *
+ */
+
+@Path("v1/status")
+public class StatusResource {
+
+    private static final Logger logger = LogManager.getLogger(StatusResource.class);
+
+    @Autowired
+    private StatusRepository statusRepository;
+
+    @GET
+    @Path("/")
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    @Secured(Privilege.DEFAULT)
+    public Response getDistrict(@QueryParam("entity") String entity) {
+        List<Status> districts;
+        try {
+            districts = statusRepository.getAllStatusByEntity(entity);
+            if (districts.isEmpty()) {
+                logger.error("No status found for the given entity.", () -> entity);
+                return Response.status(Response.Status.NOT_FOUND).entity(new Message("No status found for the given entity.")).build();
+            } else
+                return Response.status(Response.Status.OK).entity(districts).build();
+        } catch (Exception ex) {
+            logger.error("The status could not be retrieved", ex);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new Message(ex.getMessage())).build();
+        }
+
+    }
+
+}
