d88f7f80327e8a8915f408f54e23f1bc587f5578
Arnab Kr Ghosh <arnab.kr.ghosh@ericsson.com>
2017-09-20 20:28:30 +0530

    Contact Post API completed

--- sf-server/src/main/java/com/salesforce/repository/ContactRepository.java
+++ sf-server/src/main/java/com/salesforce/repository/ContactRepository.java
@@ -15,7 +15,12 @@ import org.springframework.transaction.annotation.Transactional;
 import com.salesforce.model.Contact;
 import com.salesforce.model.ContactSummary;
 import com.salesforce.rowmapper.ContactSummaryRowMapper;
+import com.salesforce.utils.ApplicationUtils;
 
+/**
+ * @author earngho
+ *
+ */
 @Component
 public class ContactRepository {
     private static final Logger logger = LogManager.getLogger(ContactRepository.class);
@@ -30,8 +35,11 @@ public class ContactRepository {
     @Value("${contact.pagesize}")
     private Long contactPageSize;
 
-    @Value("${sql.contact.sequence}")
-    private String contactSequence;
+    @Value("${sql.getSeq.byName}")
+    private String getContactSequence;
+
+    @Value("${sql.updateSeq.byName}")
+    private String updateContactSequence;
 
     @Value("${sql.contactTable.insert}")
     private String contactTableInsert;
@@ -48,10 +56,15 @@ public class ContactRepository {
         return contacts;
     }
 
+    /**
+     * @param contact
+     * @param username
+     * @return
+     * @throws Exception
+     */
     @Transactional
     public String createContact(Contact contact, String username) throws Exception {
-        logger.info(sqlMarker, contactSequence);
-        Integer contactId = jdbcTemplate.queryForObject(contactSequence, new Object[] {}, Integer.class);
+        Integer contactId = generateContactId();
         if (contact.getContactSummary() != null) {
             logger.info(sqlMarker, contactTableInsert);
             logger.info(sqlMarker, "Params {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}", () -> contactId, () -> contact.getContactSummary().getPubKey(), () -> contact.getContactSummary().getfName(), () -> contact.getContactSummary().getmName(),
@@ -71,6 +84,26 @@ public class ContactRepository {
         } else {
             throw new Exception("Contact cannot be created as basic contact data is missing...");
         }
+    }
+
+    /**
+     * @return
+     * @throws Exception
+     */
+    private Integer generateContactId() throws Exception {
+
+        logger.info(sqlMarker, getContactSequence);
+        logger.info(sqlMarker, "Params {}", () -> ApplicationUtils.CONTACT_SEQ_NAME);
+
+        Integer fetchedContactId = jdbcTemplate.queryForObject(getContactSequence, new Object[] { ApplicationUtils.CONTACT_SEQ_NAME }, Integer.class);
+        Integer newContactId = fetchedContactId + 1;
+
+        logger.info(sqlMarker, updateContactSequence);
+        logger.info(sqlMarker, "Params {} {}", () -> newContactId, () -> ApplicationUtils.CONTACT_SEQ_NAME);
+        jdbcTemplate.update(updateContactSequence, new Object[] { newContactId, ApplicationUtils.CONTACT_SEQ_NAME });
+
+        logger.debug("Contact ID generated: {}", () -> fetchedContactId);
+        return fetchedContactId;
     }
 
 }
---
+++ sf-server/src/main/java/com/salesforce/utils/ApplicationUtils.java
@@ -0,0 +1,21 @@
+package com.salesforce.utils;
+
+/**
+ * @author Arnab Kr Ghosh
+ *
+ */
+public class ApplicationUtils {
+
+    public static final String SALES_REP_SEQ_NAME = "SALES_REP_SEQ";
+    public static final String CONTACT_SEQ_NAME = "CONTACT_SEQ";
+    public static final String LEAD_SEQ_NAME = "LEAD_SEQ";
+    public static final String LEAD_ATTACHMENT_SEQ_NAME = "LEAD_ATTACHMENT_SEQ";
+    public static final String LEAD_TIMELINE_SEQ_NAME = "LEAD_TIMELINE_SEQ";
+    public static final String OPPORTUNITY_SEQ_NAME = "OPPORTUNITY_SEQ";
+    public static final String OPPORTUNITY_ATT_SEQ_NAME = "OPPORTUNITY_ATT_SEQ";
+    public static final String OPPORTUNITY_TM_SEQ_NAME = "OPPORTUNITY_TM_SEQ";
+    public static final String ACCOUNT_SEQ_NAME = "ACCOUNT_SEQ";
+    public static final String ACCOUNT_ATT_SEQ_NAME = "ACCOUNT_ATT_SEQ";
+    public static final String ACCOUNT_TM_SEQ_NAME = "ACCOUNT_TM_SEQ";
+
+}
