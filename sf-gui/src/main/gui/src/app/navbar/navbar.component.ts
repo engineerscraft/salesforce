a317307d2dc4a772aa20bfe086207c48d1189c83
Saptarshi Basu <sapb2004@gmail.com>
2017-10-14 17:58:42 +0530

    Removed the division combo on nav bar

--- sf-gui/src/main/gui/src/app/navbar/navbar.component.ts
+++ sf-gui/src/main/gui/src/app/navbar/navbar.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
-import { DivisionService } from '../division.service';
 
 @Component({
   selector: 'app-navbar',
@@ -13,28 +12,12 @@ export class NavbarComponent implements OnInit {
   private division = 'All';
   private divisions;
 
-  constructor(private router : Router, private divisionService: DivisionService) { }
+  constructor(private router : Router) { }
 
-  ngOnInit() {
-    this.divisionService.getDivisions()
-      .subscribe(
-        data => {
-          this.divisions = data;
-        }
-      );
-    if(localStorage.getItem('DIVISION_DES')) {
-      this.division = localStorage.getItem('DIVISION_DES');
-    }
-  }
+  ngOnInit() { }
 
   logOff() {
     localStorage.clear();
     this.router.navigate([""]);
-  }
-
-  setDivision(div) {
-    this.division = div.des;
-    localStorage.setItem('DIVISION_PUB_KEY', div.pubKey);
-    localStorage.setItem('DIVISION_DES', div.des);
   }
 }
