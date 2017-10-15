10d89d1171504011445305fc9bc8d5dee5aa86a5
Saptarshi Basu <sapb2004@gmail.com>
2017-10-15 21:49:52 +0530

    Build script fixed and script names changed

--- sf-server/src/main/resources/bin/start_hrms.sh
+++
@@ -1,7 +0,0 @@
-export HRMS_HOME=.
-ENV=$1
-if [ "$ENV" = "" ]
-then 
- ENV='prod';
-fi
-java -Xms256m -Xmx512m -server -Xloggc:log/gc.log -verbose:gc -XX:+PrintGCDetails -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=log -Dlogging.config=config/log4j2.xml -Dspring.config.location=config/application-$ENV.properties -jar lib/hrms.jar
---
+++ sf-server/src/main/resources/bin/start_sfm.bat
@@ -0,0 +1,6 @@
+set SFM_HOME=%cd%
+set ENV=%1
+IF [%1]==[] (
+set ENV=prod
+)
+java -Dname=sfmApp -Xms256m -Xmx512m -server -Xloggc:log/gc.log -verbose:gc -XX:+PrintGCDetails -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=log -Dlogging.config=config/log4j2.xml -Dspring.config.location=config/application-prod.properties -jar lib/sfm.jar
